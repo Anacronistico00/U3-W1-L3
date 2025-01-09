55b33cf7cf3fc48ebf544717000441a23508f7b5
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-01-09 15:06:27 +0100

    Punti extra(3-4-5) completati

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,15 +1,41 @@
 import SingleBook from './SingleBook';
+import { Component } from 'react';
 
-const BookList = ({ books }) => {
-  return (
-    <div
-      style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}
-    >
-      {books.map((book, index) => (
-        <SingleBook key={index} book={book} />
-      ))}
-    </div>
-  );
-};
+class BookList extends Component {
+  state = {
+    search: '',
+  };
+
+  handleSearch = (e) => {
+    this.setState({
+      search: e.target.value,
+    });
+  };
+
+  render() {
+    const filteredBooks = this.props.books.filter((book) =>
+      book.title.toLowerCase().includes(this.state.search.toLowerCase())
+    );
+
+    return (
+      <div>
+        <div style={{ margin: '1rem', textAlign: 'center' }}>
+          <input
+            type='text'
+            placeholder='Cerca un libro...'
+            value={this.state.search}
+            onChange={this.handleSearch}
+            style={{ padding: '0.5rem', width: '50%', fontSize: '1rem' }}
+          />
+        </div>
+        <div className='d-flex flex-wrap justify-content-center'>
+          {filteredBooks.map((book, i) => (
+            <SingleBook key={i} book={book} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default BookList;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,14 +1,38 @@
+import { Component } from 'react';
 import { Card } from 'react-bootstrap';
 
-const SingleBook = ({ book }) => {
-  return (
-    <Card style={{ width: '18rem', margin: '1rem' }}>
-      <Card.Img variant='top' src={book.img} alt={book.title} />
-      <Card.Body>
-        <Card.Title>{book.title}</Card.Title>
-      </Card.Body>
-    </Card>
-  );
-};
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
+
+  handleClick = () => {
+    this.setState({
+      selected: !this.state.selected,
+    });
+  };
+  render() {
+    return (
+      <Card
+        style={{
+          width: '16rem',
+          margin: '7px',
+          border: this.state.selected ? '1px solid red' : 'none',
+        }}
+      >
+        <Card.Img
+          variant='top'
+          style={{ height: '400px' }}
+          src={this.props.book.img}
+          alt={this.props.book.title}
+          onClick={this.handleClick}
+        />
+        <Card.Body>
+          <Card.Title>{this.props.book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
 
 export default SingleBook;

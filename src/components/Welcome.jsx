3751cbb437b512dc1fa0ee7d26b8f0e602dc6e54
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-01-09 15:11:31 +0100

    Cambiato l'alert in una modale

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,27 +1,35 @@
-import { Alert, Button } from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 import { useState } from 'react';
 
 function Welcome() {
   const [show, setShow] = useState(true);
 
-  if (show) {
-    return (
-      <Alert
-        variant='dark'
-        onClose={() => setShow(false)}
-        dismissible
-        className='text-center px-0 py-4 mb-0'
+  const handleClose = () => setShow(false);
+
+  return (
+    <>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop='static'
+        keyboard={false}
       >
-        <hr />
-        <Alert.Heading className='fs-1 fw-bolder text-black'>
-          Welcome To My Library!
-        </Alert.Heading>
-        <p>Feel free to search and inform about any book in this list</p>
-        <hr />
-      </Alert>
-    );
-  }
-  return <Button onClick={() => setShow(true)}>Show Alert</Button>;
+        <Modal.Header closeButton>
+          <Modal.Title className='fs-2 fw-bolder text-black'>
+            Welcome To My Library!
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Feel free to search and inform about any book in this list
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 export default Welcome;
